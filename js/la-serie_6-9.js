3236ebfc898af80b327bc84de6e8824e0f5e5e78
anavaira <marilinavaira@gmail.com>
2022-08-22 01:41:26 -0300

    first presentation completed

---
+++ js/la-serie_6-9.js
@@ -0,0 +1,95 @@
+const button6a9Chapter01 = document.getElementById("button__la-serie__ch01");
+
+button6a9Chapter01.addEventListener("mouseover", ()=> {
+    button6a9Chapter01.setAttribute("src", "img/la-serie_chapter01_hover.png");
+}
+)
+
+button6a9Chapter01.addEventListener("mouseout", ()=> {
+    button6a9Chapter01.setAttribute("src", "img/la-serie_chapter01.png");
+}
+)
+
+const button6a9Chapter02 = document.getElementById("button__la-serie__ch02");
+
+button6a9Chapter02.addEventListener("mouseover", ()=> {
+    button6a9Chapter02.setAttribute("src", "img/la-serie_chapter02_hover.png");
+}
+)
+
+button6a9Chapter02.addEventListener("mouseout", ()=> {
+    button6a9Chapter02.setAttribute("src", "img/la-serie_chapter02.png");
+}
+)
+
+const button6a9Chapter03 = document.getElementById("button__la-serie__ch03");
+
+button6a9Chapter03.addEventListener("mouseover", ()=> {
+    button6a9Chapter03.setAttribute("src", "img/la-serie_chapter03_hover.png");
+}
+)
+
+button6a9Chapter03.addEventListener("mouseout", ()=> {
+    button6a9Chapter03.setAttribute("src", "img/la-serie_chapter03.png");
+}
+)
+
+const button6a9Chapter04 = document.getElementById("button__la-serie__ch04");
+
+button6a9Chapter04.addEventListener("mouseover", ()=> {
+    button6a9Chapter04.setAttribute("src", "img/la-serie_chapter04_hover.png");
+}
+)
+
+button6a9Chapter04.addEventListener("mouseout", ()=> {
+    button6a9Chapter04.setAttribute("src", "img/la-serie_chapter04.png");
+}
+)
+
+const button6a9Chapter05 = document.getElementById("button__la-serie__ch05");
+
+button6a9Chapter05.addEventListener("mouseover", ()=> {
+    button6a9Chapter05.setAttribute("src", "img/la-serie_chapter05_hover.png");
+}
+)
+
+button6a9Chapter05.addEventListener("mouseout", ()=> {
+    button6a9Chapter05.setAttribute("src", "img/la-serie_chapter05.png");
+}
+)
+
+const button6a9Chapter06 = document.getElementById("button__la-serie__ch06");
+
+button6a9Chapter06.addEventListener("mouseover", ()=> {
+    button6a9Chapter06.setAttribute("src", "img/la-serie_chapter06_hover.png");
+}
+)
+
+button6a9Chapter06.addEventListener("mouseout", ()=> {
+    button6a9Chapter06.setAttribute("src", "img/la-serie_chapter06.png");
+}
+)
+
+const button6a9Chapter07 = document.getElementById("button__la-serie__ch07");
+
+button6a9Chapter07.addEventListener("mouseover", ()=> {
+    button6a9Chapter07.setAttribute("src", "img/la-serie_chapter07_hover.png");
+}
+)
+
+button6a9Chapter07.addEventListener("mouseout", ()=> {
+    button6a9Chapter07.setAttribute("src", "img/la-serie_chapter07.png");
+}
+)
+
+const button6a9Chapter08 = document.getElementById("button__la-serie__ch08");
+
+button6a9Chapter08.addEventListener("mouseover", ()=> {
+    button6a9Chapter08.setAttribute("src", "img/la-serie_chapter08_hover.png");
+}
+)
+
+button6a9Chapter08.addEventListener("mouseout", ()=> {
+    button6a9Chapter08.setAttribute("src", "img/la-serie_chapter08.png");
+}
+)
