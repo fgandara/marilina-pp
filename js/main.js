48b3478870c650d84763e390b4af30e604a259f8
anavaira <marilinavaira@gmail.com>
2022-08-22 01:00:22 -0300

    home ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+// HOME BUTTONS
+
+const button3a5 = document.getElementById("home__img__button__3-5");
+
+button3a5.addEventListener("mouseover", ()=> {
+    button3a5.setAttribute("src", "img/home_3-5_hover.png");
+}
+)
+
+button3a5.addEventListener("mouseout", ()=> {
+    button3a5.setAttribute("src", "img/home_3-5_normal.png");
+}
+)
+
+const button6a9 = document.getElementById("home__img__button__6-9");
+
+button6a9.addEventListener("mouseover", ()=> {
+    button6a9.setAttribute("src", "img/home_6-9_hover.png");
+}
+)
+
+button6a9.addEventListener("mouseout", ()=> {
+    button6a9.setAttribute("src", "img/home_6-9_normal.png");
+}
+)
+
+const buttonFamYDoc = document.getElementById("home__img__button__famydoc");
+
+buttonFamYDoc.addEventListener("mouseover", ()=> {
+    buttonFamYDoc.setAttribute("src", "img/home_famydoc_hover.png");
+}
+)
+
+buttonFamYDoc.addEventListener("mouseout", ()=> {
+    buttonFamYDoc.setAttribute("src", "img/home_famydoc_normal.png");
+}
+)
+
+
+
+
+// MENU BUTTONS
+
 const buttonLaSerie = document.getElementById("la-serie");
 
 buttonLaSerie.addEventListener("mouseover", ()=> {
